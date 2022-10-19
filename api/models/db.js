12bf96a421b6ca2d66ca0c26fa944a91b24bb547
Ruslan Mustafin <ruslanmust87@gmail.com>
2022-10-20 00:45:30 +0500

    отправка heroku v5

--- api/models/db.js
+++ api/models/db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const config = require('../../configs/config.json');
 
 // const dbUrl = 'mongodb+srv://Ruslan:170997koT@cluster0.dj8aq.mongodb.net/YourFurniture?retryWrites=true&w=majority';
-const dbUrl = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
-// const dbUrl = config.dbDocker.url;
+// const dbUrl = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
+const dbUrl = process.env.MONGODB_URL;
 
 mongoose.connect(dbUrl, {
         useNewUrlParser: true,
