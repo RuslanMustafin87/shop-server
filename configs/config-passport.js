0f96868c94c54973e9122d53f4649dfd8de5a55c
Ruslan Mustafin <ruslanmust87@gmail.com>
2022-10-19 23:43:13 +0500

    отправка heroku v4

--- configs/config-passport.js
+++ configs/config-passport.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const GithubStrategy = require('passport-github2').Strategy;
 const config = require('./config.json');
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
